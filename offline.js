8a027186ec56607e5519347565006990a2b7d79c
kapil1308 <kapil.1308@outlook.com>
2019-05-25 17:58:42 +0200

    crappy bird

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1486118666,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/loaderbg-sheet0.png",
+		"images/loader-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/bird-sheet1.png",
+		"images/background-sheet0.png",
+		"images/pipe_bottom-sheet0.png",
+		"images/pipe_top-sheet0.png",
+		"images/gameoverbox-sheet0.png",
+		"images/replay-sheet0.png",
+		"images/title-sheet0.png",
+		"images/finalscoretext.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
